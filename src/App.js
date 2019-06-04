3d5df0e84032426b779a50bfc2fa8b53537a0a03
hjjeong <hjjeong@zconverter.com>
2019-06-04 16:05:14 +0900

    Phonebook commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PhoneForm from './Components/PhoneForm';
+import PhoneInfoList from './Components/PhoneInfoList';
 
-function App() {
-  return (
-    <div>
-      <PhoneForm />
-    </div>
-  );
+class App extends React.Component {
+  id = 2
+  state = {
+    information : [
+      {
+        id : 0,
+        name : "정현중",
+        phone : '01024428051'
+      },
+      {
+        id : 1,
+        name : "최승림",
+        phone : '01065994659'
+      },
+      
+    ]
+  }
+  handleCreate = data => {
+    const {information} = this.state;
+    this.setState({
+      information : information.concat({id : this.id++, ...data})
+    })
+  }
+  render(){
+    const {information} = this.state;
+    return (
+      <div>
+        <PhoneForm 
+          onCreate = {this.handleCreate}
+        />
+        <PhoneInfoList data = {this.state.information}/>
+      </div>
+    );
+  }
 }
 
 export default App;
